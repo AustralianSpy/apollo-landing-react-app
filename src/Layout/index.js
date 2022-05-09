a89022b50ca459197f5571e987634845918a9a64
Sydney Workman <sworkman1996@gmail.com>
2022-05-09 15:50:36 -0500

    app structure

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,5 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
+import Navbar from './Navbar';
+import Header from './Header';
+import Progress from './Progress';
+import ToDoList from './ToDoList';
+
 export default function Layout() {
-    return null;
+    return (
+        <>
+            <header>
+                <Navbar />
+                <Header />
+            </header>
+            <main>
+                <Progress />
+                <ToDoList />
+            </main>
+            <footer>
+                { /* Empty, used for design purposes. */ }
+            </footer>
+        </>
+    );
 }
