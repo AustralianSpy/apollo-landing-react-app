b377f2f6091e81a05748787b703b568817d55cb0
Sydney Workman <sworkman1996@gmail.com>
2022-05-10 00:07:02 -0500

    implemented svgs

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -8,7 +8,6 @@ export default function Header() {
             <section
                 className="background-image mt-0 d-flex flex-column align-items-center justify-content-center"
             >
-                { /* Add background image. */ }
                 <h1>Shoot for the Moon</h1>
                 <button className="text-white btn btn-info">Learn More</button>
             </section>
--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,5 +1,39 @@
 import React from 'react';
 
 export default function Navbar() {
-    return <p className='dark-purple mb-0'>This is a navbar!</p>
+    return (
+        <nav className="navbar p-2 dark-purple text-white navbar-dark navbar-expand-sm">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand d-flex flex-row align-content-center justify-content-between justify-content-sm-start">
+                    <img src={`${process.env.PUBLIC_URL}/imgs/rocket-icon.svg`} alt="rocket logo" className="me-3" />
+                    <p className="me-3 mt-1 fs-1 fw-bold mb-0">Apollo</p>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarOptions" aria-controls="navbarOptions" aria-expanded="false" aria-label="toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarOptions">
+                    <ul className="navbar-nav ms-auto mb-2 align-self-sm-end">
+                        <li className="nav-item">
+                            <a href="/" className="link fw-bold d-flex flex-row justify-content-between">
+                                Schedule
+                                <img src={`${process.env.PUBLIC_URL}/imgs/vector-carrot.svg`} alt="arrow" />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/" className="link fw-bold d-flex flex-row justify-content-between">
+                                About Us
+                                <img src={`${process.env.PUBLIC_URL}/imgs/vector-carrot.svg`} alt="arrow" />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/" className="link fw-bold d-flex flex-row justify-content-between">
+                                Contact
+                                <img src={`${process.env.PUBLIC_URL}/imgs/vector-carrot.svg`} alt="arrow" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
